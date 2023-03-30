c10ea943e28ec8e1af048bb387781a3dbf88c226
Albertpilaestrada <125912097+Albertpilaestrada@users.noreply.github.com>
2023-03-30 11:23:17 +0200

    Add files via upload

---
+++ Python2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+
+    name="package_general_calc",
+    version="1.0",
+    description="general calculations package",
+    author="Alberto Pila",
+    packages=["Calculos"]
+
+)
